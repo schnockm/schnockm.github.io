48678f9235228b5b66ee83f2c6dad557faeeb1d6
schnockm <144814214+schnockm@users.noreply.github.com>
2024-01-23 18:23:45 -0500

    Update personal_main.js

--- scripts/personal_main.js
+++ scripts/personal_main.js
@@ -2,10 +2,10 @@ const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/IMG_5406.JPG") {
-    myImage.setAttribute("src", "images/IMG_5741.JPG");
+  if (mySrc === "images/IMG_5514.jpg") {
+    myImage.setAttribute("src", "images/IMG_5743.jpg");
   } else {
-    myImage.setAttribute("src", "images/IMG_5406.JPG");
+    myImage.setAttribute("src", "images/IMG_5514.jpg");
   }
 }
 

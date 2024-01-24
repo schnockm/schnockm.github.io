80d83130bf81f07bd5a2b9571ffc95289504238b
schnockm <144814214+schnockm@users.noreply.github.com>
2024-01-23 20:18:24 -0500

    Update personal_main.js

--- scripts/personal_main.js
+++ scripts/personal_main.js
@@ -18,7 +18,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = "Hello, " + myName, + "nice to meet you!";
+    myHeading.textContent = "Hello, " + myName + "nice to meet you!";
   }
 }
 

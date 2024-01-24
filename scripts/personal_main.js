a6e1ce6fadb8ededcdc70e871bd907648afa6a37
schnockm <144814214+schnockm@users.noreply.github.com>
2024-01-23 20:10:01 -0500

    Update personal_main.js

--- scripts/personal_main.js
+++ scripts/personal_main.js
@@ -9,3 +9,28 @@ myImage.onclick = () => {
   }
 }
 
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Hello, ${myName} nice to meet you!";
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = "Hello, ${myName} nice to meet you!";
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
+                    
+

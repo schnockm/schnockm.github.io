60fbd2e5455ff8b06a5c398d0109ee87b9120bcd
schnockm <144814214+schnockm@users.noreply.github.com>
2024-01-23 22:12:25 -0500

    Update personal_main.js

--- scripts/personal_main.js
+++ scripts/personal_main.js
@@ -5,21 +5,19 @@ const myHeading = document.querySelector("h1");
 
 function setUserName() {
   const myName = prompt("Please enter your name.");
-  if (!myName) {
-    setUserName();
-  } else {
+  if (myName) {
     localStorage.setItem("name", myName);
     myHeading.textContent = "Hello, " + myName + " nice to meet you!";
   }
 }
 
 function changeFontColor() {
+  // Change the font color of the title to yellow
   myHeading.style.color = "yellow";
 }
 
-function resetFontColor() {
-  myHeading.style.color = "white";
-}
+// Reset font color to white when the page loads
+myHeading.style.color = "white";
 
 if (!localStorage.getItem("name")) {
   setUserName();
@@ -34,7 +32,6 @@ myButton.onclick = function() {
 
 myColorButton.onclick = function() {
   changeFontColor();
-  setTimeout(resetFontColor, 2000); 
 };
                     
 
